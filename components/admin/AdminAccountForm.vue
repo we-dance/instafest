bd94732abbb588664da5bb41917d6b06ea81698c
Aleksey Razbakov <aleksey@razbakov.com>
2024-04-18 11:02:48 +0200

    chore: refactor admin account form

---
+++ components/admin/AdminAccountForm.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import { adminAccountShema } from '~/types/adminAccount'
+
+const { account: currentAccount, updateAccount } = useFirebaseAuth()
+
+const form = useForm({
+  validationSchema: toTypedSchema(adminAccountShema),
+  initialValues: currentAccount.value,
+})
+
+const onSubmit = form.handleSubmit(async (values: any) => {
+  try {
+    await updateAccount(values)
+  } catch (error: any) {
+    toast({
+      title: 'Error',
+      description: error.message,
+    })
+  }
+})
+</script>
+
+<template>
+  <Toaster />
+  <form @submit.prevent="onSubmit">
+    <FormField v-slot="{ componentField }" name="firstName">
+      <FormItem>
+        <FormLabel>First Name</FormLabel>
+        <FormControl>
+          <Input v-bind="componentField" />
+        </FormControl>
+        <FormDescription />
+        <FormMessage />
+      </FormItem>
+    </FormField>
+
+    <FormField v-slot="{ componentField }" name="lastName">
+      <FormItem>
+        <FormLabel>Last Name</FormLabel>
+        <FormControl>
+          <Input v-bind="componentField" />
+        </FormControl>
+        <FormDescription />
+        <FormMessage />
+      </FormItem>
+    </FormField>
+
+    <FormField v-slot="{ componentField }" name="country">
+      <FormItem>
+        <FormLabel>Country</FormLabel>
+        <Select v-bind="componentField">
+          <FormControl>
+            <SelectTrigger>
+              <SelectValue placeholder="Select Country" />
+            </SelectTrigger>
+          </FormControl>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="DE">Germany</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <FormDescription />
+        <FormMessage />
+      </FormItem>
+    </FormField>
+
+    <FormField v-slot="{ componentField }" name="email">
+      <FormItem>
+        <FormLabel>Email</FormLabel>
+        <FormControl>
+          <Input v-bind="componentField" />
+        </FormControl>
+        <FormDescription />
+        <FormMessage />
+      </FormItem>
+    </FormField>
+
+    <Button type="submit">Save</Button>
+  </form>
+</template>
--- pages/[org]/admin/settings/profile.vue
+++ pages/[org]/admin/settings/profile.vue
@@ -2,22 +2,6 @@
 definePageMeta({
   layout: 'admin',
 })
-
-import { useForm } from 'vee-validate'
-import { until } from '@vueuse/core'
-import { toTypedSchema } from '@vee-validate/zod'
-import { adminAccountShema } from '~/types/adminAccount'
-
-const { account: currentAccount, updateAccount } = useFirebaseAuth()
-
-const form = useForm({
-  validationSchema: toTypedSchema(adminAccountShema),
-  initialValues: currentAccount.value,
-})
-
-const onSubmit = form.handleSubmit(async (values) => {
-  await updateAccount(values)
-})
 </script>
 
 <template>
@@ -31,61 +15,6 @@ const onSubmit = form.handleSubmit(async (values) => {
       </div>
     </div>
 
-    <form @submit.prevent="onSubmit">
-      <FormField v-slot="{ componentField }" name="firstName">
-        <FormItem>
-          <FormLabel>First Name</FormLabel>
-          <FormControl>
-            <Input v-bind="componentField" />
-          </FormControl>
-          <FormDescription />
-          <FormMessage />
-        </FormItem>
-      </FormField>
-
-      <FormField v-slot="{ componentField }" name="lastName">
-        <FormItem>
-          <FormLabel>Last Name</FormLabel>
-          <FormControl>
-            <Input v-bind="componentField" />
-          </FormControl>
-          <FormDescription />
-          <FormMessage />
-        </FormItem>
-      </FormField>
-
-      <FormField v-slot="{ componentField }" name="country">
-        <FormItem>
-          <FormLabel>Country</FormLabel>
-          <Select v-bind="componentField">
-            <FormControl>
-              <SelectTrigger>
-                <SelectValue placeholder="Select Country" />
-              </SelectTrigger>
-            </FormControl>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="DE">Germany</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <FormDescription />
-          <FormMessage />
-        </FormItem>
-      </FormField>
-
-      <FormField v-slot="{ componentField }" name="email">
-        <FormItem>
-          <FormLabel>Email</FormLabel>
-          <FormControl>
-            <Input v-bind="componentField" />
-          </FormControl>
-          <FormDescription />
-          <FormMessage />
-        </FormItem>
-      </FormField>
-
-      <Button type="submit">Save</Button>
-    </form>
+    <AdminAccountForm />
   </div>
 </template>
