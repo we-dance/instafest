3e35a2c7f4dfc082b703515c56e1867fc4de9092
Aleksey Razbakov <aleksey@razbakov.com>
2024-04-19 19:00:23 +0200

    fix layout spacing

--- components/admin/AdminPage.vue
+++ components/admin/AdminPage.vue
@@ -6,7 +6,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="h-full flex-1 flex-col space-y-8 p-8 md:flex">
+  <div class="h-full flex-1 flex-col space-y-8 md:flex">
     <div class="flex items-center justify-between space-y-2">
       <div>
         <h2 class="text-2xl font-bold tracking-tight">{{ header }}</h2>
