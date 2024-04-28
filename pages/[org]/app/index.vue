dfdd3fce674ff831b40e98bf273ce3f052433a7d
Aleksey Razbakov <aleksey@razbakov.com>
2024-04-28 15:21:14 +0200

    show welcome message

--- pages/[org]/app/index.vue
+++ pages/[org]/app/index.vue
@@ -9,6 +9,7 @@ definePageMeta({
 
 const { orgId } = useOrganizationStore()
 const { $db } = useNuxtApp()
+const { account } = useCustomer()
 
 const events = ref([])
 
@@ -32,7 +33,33 @@ onMounted(async () => {
 </script>
 
 <template>
-  <AdminPage header="Events">
+  <AdminPage>
+    <Card class="max-w-sm">
+      <CardHeader>
+        <CardTitle>Hallo {{ account.name }}!</CardTitle>
+      </CardHeader>
+      <CardContent class="space-y-2">
+        <div class="flex items-center justify-between">
+          <span class="text-gray-600 dark:text-gray-400">Aktuelles Paket</span>
+          <span class="font-medium text-gray-900 dark:text-gray-50"
+            >Silver</span
+          >
+        </div>
+        <div class="flex items-center justify-between">
+          <span class="text-gray-600 dark:text-gray-400"
+            >Verbleibende Klassen</span
+          >
+          <span class="font-medium text-gray-900 dark:text-gray-50">4</span>
+        </div>
+        <div class="flex items-center justify-between">
+          <span class="text-gray-600 dark:text-gray-400"
+            >Verbleibende Partys</span
+          >
+          <span class="font-medium text-gray-900 dark:text-gray-50">4</span>
+        </div>
+      </CardContent>
+    </Card>
+
     <StudioEventCard v-for="event in events" :event="event" :key="event.id" />
   </AdminPage>
 </template>
